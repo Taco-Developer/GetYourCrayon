fa7091bcd21906f111c87beda20a2fe1a06a7c3a
황도영 <ghkdehdud96@naver.com>
2023-04-19 10:35:02 +0900

    chore: 백엔드 세팅파일

---
+++ back-end/coldcrayon/src/main/java/com/sevenight/coldcrayon/ColdcrayonApplication.java
@@ -0,0 +1,13 @@
+package com.sevenight.coldcrayon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColdcrayonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColdcrayonApplication.class, args);
+	}
+
+}
