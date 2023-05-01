e3abd1983251a6aad069cff94641e7ca970647fb
김경민 <starlight3775@gmail.com>
2023-05-01 11:12:13 +0900

    modify: image path

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,11 +1,10 @@
 import Image from 'next/image';
-import loopy from '../../public/images/loopy.jpg';
 
 export default function Home() {
   return (
     <main className="bg-after-noon h-screen w-screen">
       <div className="flex flex-col items-center justify-center h-screen w-screen">
-        <Image src={loopy} alt="no_img" priority />
+        <Image src="/images/loopy.jpg" alt="no_img" width={500} height={500} />
         <div>나는 행복합니다</div>
         <div>203 화이팅</div>
       </div>
