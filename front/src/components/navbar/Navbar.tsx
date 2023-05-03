d8ea04ef8547e5d857f2067ae577ed3f18b5f62e
배상현 <tkdgusdl63@naver.com>
2023-05-03 21:54:56 +0900

    feat/fe redux+HYDRATE

--- front/src/components/navbar/Navbar.tsx
+++ front/src/components/navbar/Navbar.tsx
@@ -14,10 +14,11 @@ export default function Navbar({ children }: NavbarPropsType) {
   return (
     <div>
       <NavbarDiv>
-        <Link href={'/room'} className="px-8 py-2 rounded-lg bg-main-green">
-          <div className="text-main-pink text-xs lg:text-xl sm:text-sm">
-            게임하기
-          </div>
+        <Link
+          href={'/room'}
+          className="px-8 py-2 rounded-lg bg-main-green text-main-pink text-xs lg:text-xl sm:text-sm hover:bg-main-pink hover:text-main-green"
+        >
+          게임하기
         </Link>
         <Link href={'/'}>
           <ImageDiv>
@@ -31,10 +32,11 @@ export default function Navbar({ children }: NavbarPropsType) {
             />
           </ImageDiv>
         </Link>
-        <Link href={'/board'} className="px-8 py-2 rounded-lg bg-main-green">
-          <div className="text-main-pink text-xs lg:text-xl sm:text-sm">
-            같이하기
-          </div>
+        <Link
+          href={'/board'}
+          className="px-8 py-2 rounded-lg bg-main-green text-main-pink text-xs lg:text-xl sm:text-sm hover:bg-main-pink hover:text-main-green"
+        >
+          같이하기
         </Link>
       </NavbarDiv>
       <LoginDiv>
--- front/src/store/index.ts
+++ front/src/store/index.ts
@@ -1,16 +1,42 @@
 import {
   configureStore,
-  createSlice,
-  PayloadAction,
+  Reducer,
+  AnyAction,
+  ThunkAction,
+  Action,
+  CombinedState,
   combineReducers,
 } from '@reduxjs/toolkit';
-import { persistReducer, persistStore } from 'redux-persist';
-import {
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import { HYDRATE, createWrapper } from 'next-redux-wrapper';
+import navbarSlice, { NavbarStateType } from './slice/navbarSlice';
+// import { persistReducer, persistStore } from 'redux-persist';
+// import {
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+
+export interface ReducerStates {
+  navbarPath: NavbarStateType;
+}
+
+//HYDRATE 액션을 정의 -> 슬라이스 통함
+const rootReducer = (
+  state: ReducerStates,
+  action: AnyAction,
+): CombinedState<ReducerStates> => {
+  switch (action.type) {
+    case HYDRATE:
+      return action.payload;
+    default: {
+      const combinedReducer = combineReducers({
+        navbarPath: navbarSlice.reducer,
+      });
+      return combinedReducer(state, action);
+    }
+  }
+};
