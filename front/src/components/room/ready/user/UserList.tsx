7591f81afe919149d6a1841349d8d67274262d40
이도겸 <kweony1123@naver.com>
2023-05-08 09:12:05 +0900

    css 작업

--- front/src/components/room/ready/user/UserList.tsx
+++ front/src/components/room/ready/user/UserList.tsx
@@ -31,6 +31,8 @@ const UserBody = tw.div`w-90 h-90 bg-white flex flex-col items-center justify-ce
 const TitleDiv = tw.div`w-full h-10 font-bold text-5xl flex items-center justify-center m-2`;
 const CntDiv = tw.div`w-80 h-10 font-bold text-3xl flex items-center justify-start border-black border-2 rounded-lg m-2`;
 const ListDiv = tw.div`w-90 h-70 bg-orange-200 m-2 flex flex-col items-center justify-start`;
-const UserDiv = tw.div`w-90 h-auto m-2 p-1 border-black border-2 rounded-l-full rounded-r-md flex items-center justify-start`;
+const UserDiv = tw.div`w-90 h-20 m-1 p-1 border-black border-2 rounded-l-full rounded-r-2xl flex items-center justify-start`;
 const Profile = tw.div`h-full w-20 border-black border-2 rounded-full bg-slate-100`;
 const UserName = tw.div`text-2xl m-4`;
+
+// 그리드로 적용하자
