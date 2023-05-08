2bdd092c1de16cbedb16ec5af2b13c0c870ce662
김경민 <starlight3775@gmail.com>
2023-05-08 16:44:49 +0900

    test: socker setting for server

--- node-server/index.js
+++ node-server/index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://k8b203.p.ssafy.io:3002",
     methods: ["GET", "POST"],
   },
 });
@@ -60,4 +60,4 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3001, () => console.log("SERVER RUNNING"));
+server.listen(3003, () => console.log("SERVER RUNNING"));
