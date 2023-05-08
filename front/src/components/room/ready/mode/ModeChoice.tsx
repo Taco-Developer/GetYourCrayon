d74814626adb57f49fa2ddb7773a20dc95bbdb0e
이도겸 <kweony1123@naver.com>
2023-05-08 17:50:17 +0900

    게임 대기방 css 구조잡기 #29

---
+++ front/src/components/room/ready/mode/ModeChoice.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import tw from 'tailwind-styled-components';
+
+export default function ModeChoice() {
+  return (
+    <div>
+      <h1>모드선택</h1>
+    </div>
+  );
+}
