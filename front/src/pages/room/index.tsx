f31453ed25a0a23601d14c92dd46a0467c536720
김경민 <starlight3775@gmail.com>
2023-05-08 15:47:59 +0900

    test

--- front/src/pages/room/index.tsx
+++ front/src/pages/room/index.tsx
@@ -1,31 +1,31 @@
-// 'use client';
-// import React, { useState, useEffect } from 'react';
-// import { socket } from '@/socket/server';
-// import Ready from '@/components/room/ready/Ready';
-// import tw from 'tailwind-styled-components';
+'use client';
+import React, { useState, useEffect } from 'react';
+import { socket } from '@/socket/server';
+import Ready from '@/components/room/ready/Ready';
+import tw from 'tailwind-styled-components';
 
-// export default function Room() {
-//   const [userId, setUserId] = useState<string>('');
-//   const [room, setRoom] = useState<string>('');
-//   const [status, setStatus] = useState<string>('ready');
+export default function Room() {
+  const [userId, setUserId] = useState<string>('');
+  const [room, setRoom] = useState<string>('');
+  const [status, setStatus] = useState<string>('ready');
 
-//   useEffect(() => {
-//     socket;
-//   }, []);
+  useEffect(() => {
+    socket;
+  }, []);
 
-//   switch (status) {
-//     case 'ready':
-//       return (
-//         <Ready
-//           socket={socket}
-//           userId={userId}
-//           setUserId={setUserId}
-//           room={room}
-//           setRoom={setRoom}
-//           setStatus={setStatus}
-//         />
-//       );
-//       break;
-//   }
-// }
-// // socket={socket} userId={userId} setUserId={setUserId} room={room} setRoom={setRoom}
+  switch (status) {
+    case 'ready':
+      return (
+        <Ready
+          socket={socket}
+          userId={userId}
+          setUserId={setUserId}
+          room={room}
+          setRoom={setRoom}
+          setStatus={setStatus}
+        />
+      );
+      break;
+  }
+}
+// socket={socket} userId={userId} setUserId={setUserId} room={room} setRoom={setRoom}
