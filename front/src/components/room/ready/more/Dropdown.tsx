a58ef759f43eeaa3745ceeb0f0eba6ac1d8b03e4
이도겸 <kweony1123@naver.com>
2023-05-09 17:37:35 +0900

    드롭다운 부분 다시시도

--- front/src/components/room/ready/more/Dropdown.tsx
+++ front/src/components/room/ready/more/Dropdown.tsx
@@ -1,4 +1,8 @@
+import { useState, useEffect } from 'react';
+
 export default function Dropdown() {
+  const [animation, setAnimation] = useState<boolean>(false);
+  const [repeat, setRepeat] = useState<null>(null);
   return (
     <div>
       <h1>Dropdown</h1>
