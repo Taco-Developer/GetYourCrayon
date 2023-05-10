e44c1f7029e41c621423d1b16de625548bfe147a
김경민 <starlight3775@gmail.com>
2023-05-10 14:58:34 +0900

    test: wesocketconfig 작동여부 확인

--- back-end/coldcrayon/src/main/java/com/sevenight/coldcrayon/config/WebSocketConfig.java
+++ back-end/coldcrayon/src/main/java/com/sevenight/coldcrayon/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.sevenight.coldcrayon.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -7,9 +8,11 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 
 @Configuration
 @EnableWebSocket
+@Slf4j
 public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(new WebSocketHandler(), "/chat/{roomId}").setAllowedOrigins("*");
+        log.debug("registerWebSocketHandler 사용"); 
     }
 }
