91f9124539314659874e36795089abfed88019dc
배상현 <tkdgusdl63@naver.com>
2023-05-10 10:19:48 +0900

    fix/fe 로그인 #35

--- front/src/components/login/Login.tsx
+++ front/src/components/login/Login.tsx
@@ -92,7 +92,6 @@ export default function Login() {
                 if (!KAKAO_URL) {
                   console.log(KAKAO_URL);
                   throw new Error('카카오 URL이 잘못되있습니다.');
-                  console.log(KAKAO_URL);
                 }
                 console.log(KAKAO_URL);
                 location.href = KAKAO_URL;
