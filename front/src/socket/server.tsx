ecc85480c64c58f6108a61766db7d0a441192e32
김경민 <starlight3775@gmail.com>
2023-05-10 09:46:05 +0900

    Update front/src/socket/server.tsx

--- front/src/socket/server.tsx
+++ front/src/socket/server.tsx
@@ -3,4 +3,4 @@ import io from 'socket.io-client';
 // const URL =
 //   process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3001';
 
-export const socket = io('http://k8b230.p.ssafy.io:3001');
+export const socket = io('http://k8b203.p.ssafy.io:3001');
