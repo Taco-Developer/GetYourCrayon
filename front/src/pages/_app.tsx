e4a6f921ae9d2e828c36925f4d9e7847250a3f1e
배상현 <tkdgusdl63@naver.com>
2023-05-10 13:29:52 +0900

    fix/feat 배경화면 조건 수정 #35

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -5,6 +5,7 @@ import type { NextPage } from 'next';
 import wrapper from '@/store';
 import { Provider } from 'react-redux';
 import tw from 'tailwind-styled-components';
+import { useEffect, useState } from 'react';
 
 /** 기본 Nextpage 타입에 추가로 getLayout 타입지정 */
 export type NextPageWithLayout = NextPage & {
@@ -20,20 +21,22 @@ function App({ Component, pageProps }: AppPropsWithLayoutType) {
   const { store, props } = wrapper.useWrappedStore(pageProps);
   /** getLayout이 falsy값이면 대체 함수로 page매개변수를 받는다. */
   const getLayout = Component.getLayout ?? ((page) => page);
-  const now = new Date();
-  const nowHour = now.getHours();
+  const [bgTheme, setBgtheme] = useState('');
+  useEffect(() => {
+    const date = new Date();
+    const nowHour = date.getHours();
+    const bgThe =
+      nowHour >= 6 && nowHour <= 15
+        ? 'bg-after-noon'
+        : nowHour >= 16 && nowHour <= 20
+        ? 'bg-even-ing'
+        : 'bg-night-ing';
+    setBgtheme(bgThe);
+  }, []);
   return (
     <Provider store={store}>
       {getLayout(
-        <Container
-          className={`${
-            nowHour >= 6 && nowHour <= 15
-              ? 'bg-after-noon'
-              : nowHour >= 16 && nowHour <= 20
-              ? 'bg-even-ing'
-              : 'bg-night-ing'
-          }`}
-        >
+        <Container className={bgTheme}></Container>
           <Component {...props.pageProps} />
         </Container>,
       )}
