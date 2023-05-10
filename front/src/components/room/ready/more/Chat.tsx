59a1ce4b5e6879c32a1d5453ea7b7d913213b020
이도겸 <kweony1123@naver.com>
2023-05-11 02:38:13 +0900

    fix/fe 메시지 컴포넌트 수정진행중 #56

--- front/src/components/room/ready/more/Chat.tsx
+++ front/src/components/room/ready/more/Chat.tsx
@@ -22,7 +22,6 @@ export default function Chat({
   messageList,
 }: ChatPropsType) {
   const [currentMessage, setCurrentMessage] = useState<string>('');
-  // const [messageList, setMessageList] = useState<MessageType[]>([]);
   const messagesEndRef = useRef<HTMLDivElement>(null);
 
   const scrollToBottom = () => {
@@ -40,37 +39,10 @@ export default function Chat({
     }
   };
 
-  // const sendMessage = async () => {
-  //   if (currentMessage !== '') {
-  //     const messageData: MessageType = {
-  //       author: userId,
-  //       message: currentMessage,
-  //     };
-
-  //     await socket.emit('send_message', messageData);
-  //     setMessageList((list) => [...list, messageData]);
-  //     setCurrentMessage('');
-  //   }
-  // };
-
-  // useEffect((): (() => void) => {
-  //   socket.on('receive_message', (data) => {
-  //     setMessageList((list) => [...list, data]);
-  //   });
-  //   return () => {
-  //     socket.off('receive_message');
-  //   };
-  // }, [socket]);
-
   return (
-    <div className="chat-window">
-      <div className="chat-header">
-        <p>Live Chat</p>
-      </div>
-
-      {/*  */}
-      <div className="chat-body">
-        <div className="message-container">
+    <OutDiv>
+      <ChatDiv>
+        <ChatBody>
           {messageList.map((messageContent, i) => {
             return (
               <div
@@ -89,12 +61,10 @@ export default function Chat({
               </div>
             );
           })}
-        </div>
-      </div>
-      {/*  */}
-
-      <div className="chat-footer">
-        <input
+        </ChatBody>
+      </ChatDiv>
+      <InputDiv>
+        <ChatInput
           type="text"
           value={currentMessage}
           placeholder="메시지..."
@@ -105,8 +75,66 @@ export default function Chat({
             event.key === 'Enter' && sendMessage();
           }}
         />
-        <button onClick={sendMessage}>&#9658;</button>
-      </div>
-    </div>
+        <ChatBtn>전송</ChatBtn>
+      </InputDiv>
+    </OutDiv>
   );
 }
+
+const OutDiv = tw.div`h-90 w-90 bg-white bg-opacity-50 rounded-xl flex flex-col items-center justify-center p-5`;
+const ChatDiv = tw.div`h-90 w-full flex items-center justify-center relative px-3`;
+const ChatBody = tw.div`h-full w-full overflow-y-scroll overflow-x-hidden scrollbar-ssibal`;
+const Message = tw.div`h-auto flex`;
+const MessageContent = tw.div`h-auto w-auto bg-green-500 rounded-xl text-white flex items-center m-5 break-words`;
+const YouMe = tw.div``;
+const InputDiv = tw.div`h-10 w-full flex items-center justify-around`;
+const ChatInput = tw.input`h-full w-70 bg-white rounded-xl flex items-center justify-center`;
+const ChatBtn = tw.button`h-full w-20 bg-main-green hover:bg-main-pink rounded-xl flex items-center justify-center`;
+
+// return (
+//   <div className="chat-window">
+//     <div className="chat-header">
+//       <p>Live Chat</p>
+//     </div>
+
+//     {/*  */}
+//     <div className="chat-body">
+//       <div className="message-container">
+// {messageList.map((messageContent, i) => {
+//   return (
+//     <div
+//       key={i}
+//       className="message"
+//       id={userId === messageContent.author ? 'you' : 'other'}
+//     >
+//       <div>
+//         <div className="message-meta">
+//           <p id="author">{messageContent.author}</p>
+//         </div>
+//         <div className="message-content">
+//           <p>{messageContent.message}</p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// })}
+//       </div>
+//     </div>
+//     {/*  */}
+
+//     <div className="chat-footer">
+//       <input
+//         type="text"
+//         value={currentMessage}
+//         placeholder="메시지..."
+//         onChange={(event) => {
+//           setCurrentMessage(event.target.value);
+//         }}
+//         onKeyPress={(event) => {
+//           event.key === 'Enter' && sendMessage();
+//         }}
+//       />
+//       <button onClick={sendMessage}>&#9658;</button>
+//     </div>
+//   </div>
+// );
