0af34a18cbaae8a98fe4ffee565162879c7c5c23
김경민 <starlight3775@gmail.com>
2023-05-10 17:37:09 +0900

    test: fix socket url

--- front/src/components/room/ready/Ready.tsx
+++ front/src/components/room/ready/Ready.tsx
@@ -38,7 +38,7 @@ export default function Ready({
   useEffect(() => {
     if (finalroom !== '') {
       const newClient = new W3CWebSocket(
-        `wss://getyourcrayon.co.kr/chat/${finalroom}`,
+        `wss://k8b203.p.ssafy.io/chat/${finalroom}`,
       );
       setClient(newClient);
     }
