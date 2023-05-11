ebaa51f7f91db99ce39d84b563145c2f177d7dab
김경민 <starlight3775@gmail.com>
2023-05-12 02:26:16 +0900

    chore: chatting order added

--- back-end/coldcrayon/src/main/java/com/sevenight/coldcrayon/config/WebSocketHandler.java
+++ back-end/coldcrayon/src/main/java/com/sevenight/coldcrayon/config/WebSocketHandler.java
@@ -1,11 +1,14 @@
 package com.sevenight.coldcrayon.config;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -33,6 +36,7 @@ public class WebSocketHandler extends TextWebSocketHandler {
             // roomId 키로 검색한 정보가 userScoreMap에 존재하지 않을 때 새로 만들어 suerScoreMap에 추가
             Map<WebSocketSession, Integer> scores = userScoreMap.computeIfAbsent(roomId, key -> new ConcurrentHashMap<>());
             scores.put(session, 1000);  // scores에 세션과 점수(0)을 저장: Map<세션, 점수>
+            userScoreMap.put(session.getId(), scores);
 
             // roomId에 접속중인 유저 리스트를 모두 출력
             for (Map<WebSocketSession, Integer> valueMap : userScoreMap.values()) {
@@ -57,7 +61,18 @@ public class WebSocketHandler extends TextWebSocketHandler {
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String roomId = extractRoomId(session);
         List<WebSocketSession> sessions = sessionsMap.getOrDefault(roomId, Collections.emptyList());
-        Map<WebSocketSession, Integer> scores = userScoreMap.getOrDefault(roomId, Collections.emptyMap());
+
+        Map<WebSocketSession, Integer> scores = userScoreMap.getOrDefault(roomId, Collections.emptyMap());  // <세션, 점수>
+
+        // JSON 파싱을 위한 ObjectMapper 객체 생성
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // 메시지 내용을 JSON으로 파싱하여 Map 형태로 변환
+        Map<String, String> jsonMessage = objectMapper.readValue(message.getPayload(), new TypeReference<Map<String, String>>() {});
+
+        // JSON에서 author와 message 값을 추출
+        String author = jsonMessage.get("clientId");
+        String content = jsonMessage.get("content");
 
         for (WebSocketSession s : sessions) {
             if (s.isOpen()) {
@@ -66,7 +81,7 @@ public class WebSocketHandler extends TextWebSocketHandler {
         }
 
         // 메시지 내용을 분석하여 점수를 업데이트하는 작업을 수행합니다.
-        String content = message.getPayload();
+
         if (content.equals("increase")) {
             // 점수 증가 로직
             Integer score = scores.get(session);
@@ -76,17 +91,27 @@ public class WebSocketHandler extends TextWebSocketHandler {
             Integer score = scores.get(session);
             scores.put(session, score - 1);
         }
+        else if (content.equals("show_scores")) {
+            // 업데이트된 점수를 모든 세션에게 전송합니다.
+            for (WebSocketSession s : sessions) {
+                if (s.isOpen()) {
+                    // 업데이트된 점수를 전송
+                    Integer score = scores.get(s);
+                    String scoreMessage = "Score: " + score;// ObjectMapper 객체 생성
 
-        // 업데이트된 점수를 모든 세션에게 전송합니다.
-        for (WebSocketSession s : sessions) {
-            if (s.isOpen()) {
-                // 업데이트된 점수를 전송
-                Integer score = scores.get(s);
-                String scoreMessage = "Score: " + score;
-                s.sendMessage(new TextMessage(scoreMessage));
+                    // 원하는 데이터를 JSON 형식으로 변환
+                    Map<String, String> jsonMessage2 = new HashMap<>();
+                    jsonMessage2.put("clientId", "현재 세션의 점수");
+                    jsonMessage2.put("content", scoreMessage);
+                    String json = objectMapper.writeValueAsString(jsonMessage2);
+
+                    // WebSocket 메시지로 전송
+                    s.sendMessage(new TextMessage(json));
+                }
             }
         }
     }
+
 
     // 유저의 소켓 연결이 끊어졌을 때 실행
     @Override
