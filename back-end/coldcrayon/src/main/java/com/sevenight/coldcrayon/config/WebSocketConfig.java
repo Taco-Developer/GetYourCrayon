230b57ee48d40965813e03ba3efb202874cb521a
황도영 <ghkdehdud96@naver.com>
2023-05-11 14:40:24 +0900

    fix : path 수정

--- back-end/coldcrayon/src/main/java/com/sevenight/coldcrayon/config/WebSocketConfig.java
+++ back-end/coldcrayon/src/main/java/com/sevenight/coldcrayon/config/WebSocketConfig.java
@@ -10,6 +10,6 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new WebSocketHandler(), "/chat/{roomId}").setAllowedOrigins("*");
+        registry.addHandler(new WebSocketHandler(), "/{roomId}").setAllowedOrigins("*");
     }
 }
