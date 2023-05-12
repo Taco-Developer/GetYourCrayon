709200fa2618d53557cc2e7c97d1152b813dedba
송대현 <goflwla92@naver.com>
2023-05-12 09:15:13 +0900

    feat: be/ WebSocketConfig에 RequiredArgsConstructor 어노테이션 붙어있음

--- back-end/coldcrayon/src/main/java/com/sevenight/coldcrayon/config/WebSocketConfig.java
+++ back-end/coldcrayon/src/main/java/com/sevenight/coldcrayon/config/WebSocketConfig.java
@@ -1,5 +1,7 @@
 package com.sevenight.coldcrayon.config;
 
+import com.sevenight.coldcrayon.room.service.RoomService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -7,7 +9,9 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 
 @Configuration
 @EnableWebSocket
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketConfigurer {
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(new WebSocketHandler(), "/chat/{roomId}").setAllowedOrigins("*");
