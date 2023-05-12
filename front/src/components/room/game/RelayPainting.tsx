fdfb275e208e2f86a190f57755b3dd3b41944433
Deokho Kim <demiacan@naver.com>
2023-05-12 10:10:09 +0900

    feat: FE/ client 컴포넌트로 전달 #62

--- front/src/components/room/game/RelayPainting.tsx
+++ front/src/components/room/game/RelayPainting.tsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
-import Painting from './roles/Painting';
+
+import { w3cwebsocket as W3CWebsocket } from 'websocket';
+
+import Drawing from './roles/Drawing';
 import Solving from './roles/Solving';
+import { useAppSelector } from '@/store/thunkhook';
 
-export default function RelayPainting() {
-  const [role, setRole] = useState('solving');
+export default function RelayPainting({ client }: { client: W3CWebsocket }) {
+  const { userRole } = useAppSelector((state) => state.userData);
 
-  switch (role) {
+  switch (userRole) {
     case 'painting':
-      return <Painting />;
+      return <Drawing client={client} />;
     case 'solving':
-      return <Solving isReverseGame={false} />;
+      return <Solving isReverseGame={false} client={client} />;
     default:
       return <div>Something wrong!!!</div>;
   }
