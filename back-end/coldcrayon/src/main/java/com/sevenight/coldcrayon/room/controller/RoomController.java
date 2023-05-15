ceb179a88a7f469478ed4a2f71de0183c8601e1e
이수민 <sumin940305@gmail.com>
2023-05-15 12:40:07 +0900

    fix/be roomController Authorization 수정

--- back-end/coldcrayon/src/main/java/com/sevenight/coldcrayon/room/controller/RoomController.java
+++ back-end/coldcrayon/src/main/java/com/sevenight/coldcrayon/room/controller/RoomController.java
@@ -29,9 +29,8 @@ public class RoomController {
     public ResponseEntity<?> createRoom(@RequestHeader String Authorization){
 
         System.err.println("컨트롤러 진입");
-//        UserDto user = authService.selectOneMember(HeaderUtil.getAccessTokenString(Authorization));
-//        System.err.println(user);
-        UserDto user = UserDto.builder().userIdx(1L).userEmail("1번@naver.com").userPoint(0).userNickname("바보").build();
+        UserDto user = authService.selectOneMember(HeaderUtil.getAccessTokenString(Authorization));
+//        UserDto user = UserDto.builder().userIdx(1L).userEmail("1번@naver.com").userPoint(0).userNickname("바보").build();
         Map<String, Object> data = roomService.saveRoom(user);
 
         return ResponseEntity.badRequest().body(data);
@@ -39,11 +38,8 @@ public class RoomController {
 
     @PostMapping("/join")
     public ResponseEntity<RoomResponseDto> joinRoom(@RequestHeader String Authorization, @RequestBody String roomIdx){
-
-//        UserDto user = authService.selectOneMember(HeaderUtil.getAccessTokenString(Authorization));
-
-//        System.err.println("dmasdfkeaiwofhjiodsa;fjioa;ejfklasd;jfioa;ewjf");
-        UserDto user = UserDto.builder().userIdx(2L).userEmail("2번@naver.com").userPoint(0).userNickname("바보2").build();
+        UserDto user = authService.selectOneMember(HeaderUtil.getAccessTokenString(Authorization));
+//        UserDto user = UserDto.builder().userIdx(2L).userEmail("2번@naver.com").userPoint(0).userNickname("바보2").build();
 
         RoomResponseDto roomResponseDto = roomService.joinRoom(user, roomIdx);
 
@@ -52,23 +48,21 @@ public class RoomController {
 
     @GetMapping("/get-user-list")
     public ResponseEntity<?> getUserList(@RequestHeader String Authorization, @RequestBody String roomIdx){
-        System.err.println("컨트롤러 진입");
         return ResponseEntity.ok().body(roomService.getUserList(roomIdx));
     }
 
 
     @PostMapping("/out")
-    public String outRoom(@RequestHeader String Authorization){
+    public RoomResponseDto outRoom(@RequestHeader String Authorization){
 
-//        UserDto user = authService.selectOneMember(HeaderUtil.getAccessTokenString(Authorization));
-        UserDto user = UserDto.builder().userIdx(1L).userEmail("1번@naver.com").userPoint(0).userNickname("바보").build();
+        UserDto user = authService.selectOneMember(HeaderUtil.getAccessTokenString(Authorization));
         return roomService.outRoom(user);
     }
 
     @PatchMapping("/change-admin")
     public RoomResponseDto changeAdmin(@RequestHeader String Authorization, @RequestBody RoomRequestDto roomRequestDto){
-//        UserDto user = authService.selectOneMember(HeaderUtil.getAccessTokenString(Authorization));
-        UserDto user = UserDto.builder().userIdx(1L).userEmail("1번@naver.com").userPoint(0).userNickname("바보").build();
+        UserDto user = authService.selectOneMember(HeaderUtil.getAccessTokenString(Authorization));
+//        UserDto user = UserDto.builder().userIdx(1L).userEmail("1번@naver.com").userPoint(0).userNickname("바보").build();
 
         return roomService.changeAdminUser(user, roomRequestDto.getRoomIdx(), roomRequestDto.getToUserIdx());
     }
@@ -76,8 +70,8 @@ public class RoomController {
     @PatchMapping("/maxuser")
     public RoomResponseDto changeMaxUser(@RequestHeader String Authorization, @RequestBody RoomRequestDto roomRequestDto){
 
-//        UserDto user = authService.selectOneMember(HeaderUtil.getAccessTokenString(Authorization));
-        UserDto user = UserDto.builder().userIdx(1L).userEmail("1번@naver.com").userPoint(0).userNickname("바보").build();
+        UserDto user = authService.selectOneMember(HeaderUtil.getAccessTokenString(Authorization));
+//        UserDto user = UserDto.builder().userIdx(1L).userEmail("1번@naver.com").userPoint(0).userNickname("바보").build();
         return roomService.changeMaxUser(user, roomRequestDto.getRoomIdx(), roomRequestDto.getRoomMax());
 
     }
