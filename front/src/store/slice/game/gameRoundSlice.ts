bee749780ca3dc6540492e88c231de03ec9688ff
Deokho Kim <demiacan@naver.com>
2023-05-16 23:55:49 +0900

    feat: FE/ redux 관리 #93

--- front/src/store/slice/game/gameRoundSlice.ts
+++ front/src/store/slice/game/gameRoundSlice.ts
@@ -3,26 +3,41 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 export interface GameRoundType {
   now: number;
   total: number;
+  isRoundStarted: boolean;
 }
 
-const INIT_NOW = 1;
-const INIT_TOTAL = 6;
-
 const initialState: GameRoundType = {
-  now: INIT_NOW,
-  total: INIT_TOTAL,
+  now: 1,
+  total: 4,
+  isRoundStarted: false,
 };
 
 const gameRoundSlice = createSlice({
   name: 'gameRound',
   initialState,
   reducers: {
+    /** 총 라운드 설정 */
+    setTotalRound(state, action: PayloadAction<number>) {
+      state.total = action.payload;
+    },
+
     /** 다음 라운드 */
     goNextRound(state) {
-      state.now++;
+      state.now += 1;
+    },
+
+    /** 라운드 시작 */
+    startRound(state) {
+      state.isRoundStarted = true;
+    },
+
+    /** 라운드 종료 */
+    endRound(state) {
+      state.isRoundStarted = false;
     },
   },
 });
 
-export const { goNextRound } = gameRoundSlice.actions;
+export const { goNextRound, endRound, setTotalRound, startRound } =
+  gameRoundSlice.actions;
 export default gameRoundSlice.reducer;
