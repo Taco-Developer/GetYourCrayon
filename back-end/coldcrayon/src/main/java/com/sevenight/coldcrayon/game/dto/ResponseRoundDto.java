4136c5e12cfae6ef290d3beabe24939853666c09
이수민 <sumin940305@gmail.com>
2023-05-17 09:20:39 +0900

    feat/be ResponseRoundDto 추가

---
+++ back-end/coldcrayon/src/main/java/com/sevenight/coldcrayon/game/dto/ResponseRoundDto.java
@@ -0,0 +1,24 @@
+package com.sevenight.coldcrayon.game.dto;
+
+import java.util.List;
+
+import com.sevenight.coldcrayon.room.dto.UserHashResponseDto;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ResponseRoundDto {
+
+    String status;
+
+    String message;
+
+    Long winnerUserIdx;
+
+    List<UserHashResponseDto> userList;
+
+}
