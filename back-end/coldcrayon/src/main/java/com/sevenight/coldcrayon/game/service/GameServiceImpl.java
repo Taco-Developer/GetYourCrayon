0bcba4e8cb57feae2f2a202b395b53cd2e8ad9cc
이수민 <sumin940305@gmail.com>
2023-05-17 09:22:31 +0900

    feat/be Round 기능 Service 코드 추가

--- back-end/coldcrayon/src/main/java/com/sevenight/coldcrayon/game/service/GameServiceImpl.java
+++ back-end/coldcrayon/src/main/java/com/sevenight/coldcrayon/game/service/GameServiceImpl.java
@@ -2,9 +2,12 @@ package com.sevenight.coldcrayon.game.service;
 
 import com.sevenight.coldcrayon.auth.dto.UserDto;
 import com.sevenight.coldcrayon.game.dto.GameRequestDto;
+import com.sevenight.coldcrayon.game.dto.RequestRoundDto;
 import com.sevenight.coldcrayon.game.dto.ResponseGameDto;
+import com.sevenight.coldcrayon.game.dto.ResponseRoundDto;
 import com.sevenight.coldcrayon.game.repository.GameRepository;
 import com.sevenight.coldcrayon.joinlist.service.JoinListService;
+import com.sevenight.coldcrayon.room.dto.UserHashResponseDto;
 import com.sevenight.coldcrayon.room.entity.RoomHash;
 import com.sevenight.coldcrayon.room.entity.RoomStatus;
 import com.sevenight.coldcrayon.room.entity.UserHash;
@@ -140,6 +143,171 @@ public class GameServiceImpl implements GameService{
         return responseGameDto;
 
     }
+
+    public ResponseRoundDto endRound(RequestRoundDto requestRoundDto){
+        String status = "fail";
+        String message;
+        ResponseRoundDto responseRoundDto = new ResponseRoundDto();
+        Optional<RoomHash> optionalRoomHash = roomRepository.findById(requestRoundDto.getRoomIdx());
+        System.err.println("서비스 진입");
+        if(optionalRoomHash.isPresent()){
+            RoomHash roomHash = optionalRoomHash.get();
+            status = "success";
+            message = "유저 포인트 변경 내역입니다.";
+            List<Object> userList = joinListService.getJoinList(roomHash.getRoomIdx());
+            System.err.println("if문 진입");
+            List<UserHashResponseDto> userHashResponseDtoList = new ArrayList<>();
+
+            for(Object user: userList){
+                Optional<UserHash> optionalUserHash = userHashRepository.findById(Long.parseLong(user.toString()));
+                if(optionalUserHash.isPresent()){
+                    UserHash userHash = optionalUserHash.get();
+                    System.err.println(userHash);
+                    userHash.setUserScore(userHash.getUserScore() + 3);
+                    if(userHash.getUserIdx().equals(requestRoundDto.getWinner())){
+                        userHash.setUserScore(userHash.getUserScore() + 3);
+                        responseRoundDto.setWinnerUserIdx(userHash.getUserIdx());
+                    }
+
+                    userHashRepository.save(userHash);
+                    userHashResponseDtoList.add(UserHashResponseDto.of(userHash));
+                }
+            }
+            System.err.println("여기까지 통과1");
+            responseRoundDto.setUserList(userHashResponseDtoList);
+            System.err.println("여기까지 통과2");
+        } else{
+            message = "조회한 방이 없습니다.";
+        }
+        System.err.println("여기까지 통과3");
+        responseRoundDto.setMessage(message);
+        System.err.println("여기까지 통과4");
+        responseRoundDto.setStatus(status);
+        System.err.println("여기까지 통과5");
+        return responseRoundDto;
+    }
+
+    public ResponseGameDto nextRound(RequestRoundDto requestRoundDto) throws IOException {
+
+        ResponseGameDto responseGameDto = new ResponseGameDto();
+        String status = "fail";
+        String message;
+
+        List<Object> userList = joinListService.getJoinList(requestRoundDto.getRoomIdx());
+        Optional<RoomHash> optionalRoomHash = roomRepository.findById(requestRoundDto.getRoomIdx());
+
+        if(optionalRoomHash.isPresent()) {
+            RoomHash roomHash = optionalRoomHash.get();
+
+            roomHash.setNowRound(roomHash.getNowRound() + 1);
+            if (roomHash.getNowRound() > roomHash.getMaxRound()) {
+                message = "최대 라운드 초과";
+            } else {
+                message = "다음 라운드 정보";
+                status = "success";
+
+                roomRepository.save(roomHash);
+
+                ThemeCategory[] themeCategories = ThemeCategory.values();
+                ThemeCategory themeCategory = themeCategories[random.nextInt(themeCategories.length)];
+                List<String> keywords = themeService.getThemeKeyword(themeCategory);
+                message = keywords.get(1) + keywords.get(0);
+                String translateScript = webClientService.papagoPost(message);
+                //             game 모드가 AI라면,,,
+
+                responseGameDto.setTheme(themeCategory);
+                responseGameDto.setCorrect(keywords.get(1));
+
+
+                switch (roomHash.getGameCategory()) {
+                    case AI:
+                        List<Object> dalEResponse = webClientService.AiPost(translateScript);
+
+                        Long i = 0L;
+                        List<String> dests = new ArrayList<>();
+                        for (Object element : dalEResponse) {
+                            Map<String, String> urlMap = (Map) element;
+                            String url = urlMap.get("url");
+
+                            String destination = roomHash.getRoomIdx() + "/" + roomHash.getGameCnt() + "/" + roomHash.getNowRound();
+                            System.err.println(url);
+                            System.err.println(destination);
+                            String dest = saveImageService.downloadImage(url, destination, ++i);
+                            dests.add(dest);
+                        }
+
+                        responseGameDto.setUrlList(dests);
+                        break;
+                    case LIAR:
+                        break;
+                    case CATCH:
+                        break;
+                    case RELAY:
+                        break;
+                    case REVERSE:
+                        break;
+                    default:
+                        System.err.println("게임 모드 없어~~~~~~~~~~~~~~~~");
+                        return null;
+                }
+
+                // 그림 그리는 유저 1명 지정하기
+                Object user = userList.get(random.nextInt(userList.size()));
+
+                responseGameDto.setSelectedUserIdx(Long.parseLong(user.toString()));
+                responseGameDto.setUserList(userList);
+            }
+        } else {
+            message = "방이 없어요.";
+        }
+
+        responseGameDto.setStatus(status);
+        responseGameDto.setMessage(message);
+
+        return responseGameDto;
+    }
+
+//    public void endGame(){
+//
+//        GameEndDto gameEndDto = new GameEndDto();
+//
+////        List<Object> userList = joinListService.getJoinList(requestRoundDto.getRoomIdx());
+////        Optional<RoomHash> optionalRoomHash = roomRepository.findById(requestRoundDto.getRoomIdx());
+//        if(optionalRoomHash.isPresent()) {
+//            List<UserHashResponseDto> userHashResponseDtoList = new ArrayList<>();
+//
+//            RoomHash roomHash = optionalRoomHash.get();
+//
+//            gameEndDto.setMessage("게임 끝");
+//            gameEndDto.setStatus("success");
+//            roomHash.setRoomStatus(RoomStatus.Ready);
+//            roomRepository.save(roomHash);
+//
+//            for(Object user: userList){
+//                Optional<UserHash> optionalUserHash = userHashRepository.findById(Long.parseLong(user.toString()));
+//                if(optionalUserHash.isPresent()){
+//                    UserHash userHash = optionalUserHash.get();
+//                    userHash.setUserPoint(userHash.getUserScore() + userHash.getUserPoint());
+//                    userHashResponseDtoList.add(UserHashResponseDto.of(userHash));
+//
+//                    userHash.setUserScore(0);
+//                    userHashRepository.save(userHash);
+//                }
+//            }
+//            String topDir = "/getchacrayon/image/history/"+ roomHash.getRoomIdx() + roomHash.getGameCnt();
+//            File dir = new File(topDir);
+//            File subdirs[] = dir.listFiles();
+//            for(int i=0; i<subdirs.length; i++){
+////                File
+//            }
+//
+//            gameEndDto.setUserHashResponseDtoList(userHashResponseDtoList);
+//
+//        } else {
+//            gameEndDto.setMessage("방이 없어요.");
+//        }
+
+
 }
 
 
