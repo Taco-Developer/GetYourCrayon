a5c6a944c268893c70a617322192181f8bee6a01
Deokho Kim <demiacan@naver.com>
2023-05-18 03:42:33 +0900

    feat: FE/ 결과 구현 #93

--- front/src/components/room/result/GameResult.tsx
+++ front/src/components/room/result/GameResult.tsx
@@ -1,25 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Image from 'next/image';
 
 import tw from 'tailwind-styled-components';
 
 import Margin, { MarginType } from '@/components/ui/Margin';
-import { useAppSelector } from '@/store/thunkhook';
+import { useAppDispatch, useAppSelector } from '@/store/thunkhook';
 import { Button } from '@/components/ui/Button';
 import { sendMessage } from '@/socket/messageSend';
+import { setGameUsers } from '@/store/slice/game/gameUsersSlice';
+import { listenEvent, removeEvent } from '@/socket/socketEvent';
+
+interface UrlListType {
+  [key: number]: string[];
+}
 
 export default function GameResult({ socket }: { socket: WebSocket }) {
-  const { gameUsers } = useAppSelector((state) => state);
+  const {
+    gameUsers,
+    roomInfo: { maxRound, roomMax },
+  } = useAppSelector((state) => state);
+  const dispatch = useAppDispatch();
+  const [urlList, setUrlList] = useState<UrlListType>();
+  const [urlKey, setUrlKey] = useState(0);
+  const [showDatas, setShowDatas] = useState<string[][]>([]);
+
+  // socket 이벤트 등록 및 전송
+  useEffect(() => {
+    if (!socket) return;
+    const messageHandler = (event: MessageEvent) => {
+      const data = JSON.parse(event.data);
+      if (data.type !== 'gameOver') return;
+
+      console.log(data);
+
+      const { userList, urlList } = data;
+      dispatch(setGameUsers(userList));
+      setUrlList(urlList);
+    };
+
+    listenEvent(socket, messageHandler);
+
+    sendMessage(socket, 'gameOver');
+
+    return () => {
+      removeEvent(socket, messageHandler);
+    };
+  }, [socket, dispatch]);
+
+  // 1초마다 urlIdx 1 증가
+  useEffect(() => {
+    if (!urlList) return;
+    if (maxRound === urlKey) return;
+
+    const timer = setInterval(() => {
+      setUrlKey((prevKey) => prevKey + 1);
+    }, 1500);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [urlList, maxRound, urlKey]);
+
+  // urlIdx가 증가하면 showDatas 추가
+  useEffect(() => {
+    if (urlKey < 1 || !urlList) return;
+    setShowDatas((prev) => [...prev, urlList[urlKey]]);
+  }, [urlList, urlKey]);
 
   return (
     <Container>
       <main className="flex-auto flex gap-6 overflow-hidden">
         <LeftSection>
-          <Margin type={MarginType.height} size={16} />
           <div className="flex justify-center gap-2 text-2xl">
             <h2>플레이어</h2>
-            <span>6/6</span>
+            <span>
+              {gameUsers.length} / {roomMax}
+            </span>
           </div>
           <Margin type={MarginType.height} size={40} />
           <UserList>
@@ -39,39 +96,26 @@ export default function GameResult({ socket }: { socket: WebSocket }) {
               </UserItem>
             ))}
           </UserList>
-          <Margin type={MarginType.height} size={40} />
         </LeftSection>
         <RightSection>
-          <div className="flex-auto rounded-xl bg-[#5AAEFC] p-4 overflow-y-auto">
-            <h2 className="text-2xl">결과 보기</h2>
-            <Margin type={MarginType.height} size={16} />
-            <ul className="">
-              {gameUsers.map(
-                ({ userProfile, userNickname, userScore, userIdx }) => (
-                  <li key={userIdx}>
-                    <div className="flex items-center">
-                      <UserProfile>
-                        <Image
-                          src={userProfile}
-                          alt="프로필"
-                          fill
-                          sizes="100%"
-                          priority
-                        />
-                      </UserProfile>
-                      <Margin type={MarginType.width} size={8} />
-                      <span>{userNickname}</span>
-                      <Margin type={MarginType.width} size={16} />
-                      <span className="text-2xl">{userScore}</span>
-                    </div>
-                  </li>
-                ),
-              )}
+          <div className="h-full rounded-xl bg-[#5AAEFC] px-8 py-6">
+            <h2 className="text-2xl">게임 결과</h2>
+            <Margin type={MarginType.height} size={24} />
+            <ul className="h-full flex flex-col gap-6 overflow-y-auto">
+              {showDatas.map((url, idx) => (
+                <li key={idx} className="">
+                  <div>{url[0]}</div>
+                  <div>{url[1]}</div>
+                  <div>{url[2]}</div>
+                  <div>{url[3]}</div>
+                </li>
+              ))}
             </ul>
+            <div></div>
           </div>
           <div className="flex justify-center">
             <Button
-              px={6}
+              px={8}
               py={2}
               rounded="2xl"
               color="bg-[#5AAEFC]"
@@ -84,7 +128,7 @@ export default function GameResult({ socket }: { socket: WebSocket }) {
             </Button>
             <Margin type={MarginType.width} size={40} />
             <Button
-              px={6}
+              px={8}
               py={2}
               color="bg-amber-500"
               rounded="2xl"
@@ -120,6 +164,8 @@ const Container = tw.div`
 const LeftSection = tw.div`
     w-1/4 
     
+    p-6
+
     bg-[#5AAEFC] 
     rounded-xl 
 
@@ -138,16 +184,13 @@ const RightSection = tw.div`
 const UserList = tw.ul`
     flex-auto
     
-    px-2 
-    
     flex 
     flex-col 
     justify-around
+    gap-6
 `;
 
 const UserItem = tw.li`
-    w-full
-
     p-2
 
     flex
@@ -155,15 +198,17 @@ const UserItem = tw.li`
     gap-2
 
     outline
-    outline-1
+    outline-2
     outline-white
     rounded-l-full
 `;
 
 const UserProfile = tw.div`
-    w-[40px] 
-    h-[40px] 
+    w-[48px] 
+    h-[48px] 
 
     bg-white 
     rounded-full
+
+    relative
 `;
